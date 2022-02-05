86f6049d5df1823b6ae2bc07964d46a70e1218cd
canderson <cole@cairene.net>
2022-02-04 17:20:34 -0800

    Initial Commit

---
+++ commands.cpp
@@ -0,0 +1,112 @@
+// $Id: commands.cpp,v 1.27 2022-01-28 18:11:56-08 - - $
+
+#include "commands.h"
+#include "debug.h"
+
+const command_hash cmd_hash {
+   {"cat"   , fn_cat   },
+   {"cd"    , fn_cd    },
+   {"echo"  , fn_echo  },
+   {"exit"  , fn_exit  },
+   {"ls"    , fn_ls    },
+   {"lsr"   , fn_lsr   },
+   {"make"  , fn_make  },
+   {"mkdir" , fn_mkdir },
+   {"prompt", fn_prompt},
+   {"pwd"   , fn_pwd   },
+   {"rm"    , fn_rm    },
+   {"rmr"   , fn_rmr   },
+};
+
+command_fn find_command_fn (const string& cmd) {
+   // Note: value_type is pair<const key_type, mapped_type>
+   // So: iterator->first is key_type (string)
+   // So: iterator->second is mapped_type (command_fn)
+   DEBUGF ('c', "[" << cmd << "]");
+   const auto result {cmd_hash.find (cmd)};
+   if (result == cmd_hash.end()) {
+      throw command_error (cmd + ": no such command");
+   }
+   return result->second;
+}
+
+command_error::command_error (const string& what):
+            runtime_error (what) {
+}
+
+int exit_status_message() {
+   int status {exec::status()};
+   cout << exec::execname() << ": exit(" << status << ")" << endl;
+   return status;
+}
+
+
+void fn_cat (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_cd (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_echo (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+   cout << word_range (words.cbegin() + 1, words.cend()) << endl;
+}
+
+void fn_exit (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+   throw ysh_exit();
+}
+
+void fn_ls (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+   DEBUGS ('l', 
+      const auto& dirents = state.get_root()->get_dirents();
+      for (const auto& entry: dirents) {
+         cerr << "\"" << entry.first << "\"->" << entry.second << endl;
+      }
+    );
+}
+
+void fn_lsr (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+
+void fn_make (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_mkdir (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_prompt (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_pwd (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_rm (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
+void fn_rmr (inode_state& state, const wordvec& words) {
+   DEBUGF ('c', state);
+   DEBUGF ('c', words);
+}
+
